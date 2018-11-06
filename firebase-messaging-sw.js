df95fe38a8563f6cb33c2b7f4237238424f47549
uib2usbt <44798885+uib2usbt@users.noreply.github.com>
2018-11-06 17:01:38 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.7/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.7/firebase-messaging.js');
 
 firebase.initializeApp({
     apiKey: "AIzaSyDTSx6b1Yw93zZ2x-6gXopLUsDvBQdLS08",
